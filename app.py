cb84e1245e0fb1ccb26aa966786e10b404a73472
Vivekan-S <106756989+Vivekan-S@users.noreply.github.com>
2023-01-03 22:05:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -12,28 +13,11 @@ img_height,img_width=180,180
 
 
 
-model_flower = load_model('model_flower.h5')
+model_flower = keras.models.load_model('model_flower.h5')
 
 
-from tensorflow.keras.models import Sequential
-num_classes = 5
 
-model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes,activation='softmax')
-])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
 
 
 
